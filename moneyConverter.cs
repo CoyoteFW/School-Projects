2e2857bdb81a91178a2e2b26fab968129ff53235
Casey J. Cox <caseyjc93@gmail.com>
2017-09-06 12:53:31 -0400

    Add files via upload

---
+++ moneyConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cox_MoneyConverter
+{
+    class moneyConverter
+    {
+        //  Declare instance variable
+        private decimal dollar;
+
+        //  Constructor
+        public moneyConverter(decimal initDollar)
+        {
+            Dollar = initDollar;
+        }
+
+        //  Property to access variable
+        public decimal Dollar
+        {
+            get
+            {
+                return dollar;
+            }
+            set
+            {
+                //  Negative check
+                if (value >= 0)
+                    dollar = value;
+            }
+        }
+        //  Method to convert to Euro
+        public decimal ToEuro()
+        {
+            decimal amt = Dollar * 0.9m;
+            return amt;
+        }
+
+        //  Method to convert to Pound
+        public decimal ToPound()
+        {
+            decimal amt = Dollar * 0.79m;
+            return amt;
+        }
+    }
+}
---
+++ moneyConverterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cox_MoneyConverter
+{
+    class moneyConverterTest
+    {
+        static void Main(string[] args)
+        {
+            decimal quantity = 0;
+            decimal convertEuro = 0;
+            decimal convertPound = 0;
+            //  Prompt user
+            Console.Write("Enter amount in US dollar: $");
+
+            //  Use input to create object of Convert
+            moneyConverter amount = new moneyConverter(quantity = decimal.Parse(Console.ReadLine()));
+            amount.Dollar = quantity;   //  set accessor
+
+            //  Calculate conversion
+            convertEuro = amount.ToEuro();
+            convertPound = amount.ToPound();
+            Console.WriteLine($"\nEquivalent amount in Euro: EUR {convertEuro}");
+            Console.WriteLine($"\nEquivalent amount in Pound: GBP {convertPound}");
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
