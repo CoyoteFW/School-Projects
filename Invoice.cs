649b0ff3c48aac8442ad9b2dd25254bb06bab236
Casey J. Cox <caseyjc93@gmail.com>
2017-09-06 12:54:39 -0400

    Add files via upload

---
+++ Invoice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//  Casey Cox
+//  C# Programming
+//  Declare an instance variable quantity type (int). Quantity of item being purchased.
+//  Define a constructor that needs one initial quantity value in (). Initializes instance quantity
+//  Define a property Quantity with get and set accessors. In set, modify value of instance only
+//      if passed value is not negative.
+//  Define a method GetInvoiceAmount that calculates and returns invoice amount. Assume unit price is
+//      $0.75. Invoice is quantity x price. Use decimals for variables and values related to money.
+
+namespace InvoiceApp
+{
+    class Invoice
+    {
+        //  Declaring instance variable
+        private int quantity;
+
+        //  Constructor
+        public Invoice(int initQuantity)
+        {
+            Quantity = initQuantity;
+        }
+
+        //  Property for instance variable quantity
+        public int Quantity
+        {
+            //  Getting the quantity
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                //  Check if value is negative or not
+                if (value >= 0)
+                    quantity = value;
+            }
+        }
+
+        //  Method to calculate invoice amount
+        public decimal GetInvoiceAmount()
+        {
+            decimal amt = Quantity * 0.75m;
+            return amt;
+        }
+    }
+}
---
+++ InvoiceTester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//  Ask user to enter quantity of item purchased
+//  Use the input quantity to create an invoice object by calling the constructor of Invoice class
+//  Display current value stored in the quantity instance variable by calling the get accessor implicitly
+//  Calculate and display the incoive amount by calling the GetINvoiceAmount method. Use string int.
+//      to format as a currency.
+
+namespace InvoiceApp
+{
+    class InvoiceTester
+    {
+        static void Main(string[] args)
+        {
+            int quantity;
+            decimal amount;
+            //  Ask user to enter quantity of item purchased.
+            Console.Write("Please enter quantity of the part you want to order: ");
+
+            //  Use input quantity to create an object of Invoice.
+            Invoice order = new Invoice(quantity = int.Parse(Console.ReadLine()));
+            order.Quantity = quantity; //   set order.Quantity to input value
+
+            //  Retrieve and display current quantity value.
+            
+            Console.WriteLine($"Quantity ordered: {order.Quantity}");
+
+            //  Calculate invoice amount.
+            amount = order.GetInvoiceAmount();
+            Console.WriteLine("Total price is: {0:C}",
+                amount);
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
